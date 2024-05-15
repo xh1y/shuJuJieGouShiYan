b23e20e8c1966d9fa95111d76790f3b0b7f9f113
ifUsernameIsTooLongNobodyWantToSeeItAll <xhily_@outlook.com>
2024-05-15 08:28:20 +0800

    delete exe

--- Exp7.cpp
+++ Exp7.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <queue>
 
@@ -22,43 +23,113 @@ typedef vector<vec_int> vec_vec_int;
 typedef vector<Group> vec_grp;
 
 // typedef struct vec_vec_int {
-//     vec_int points; 
+    // vec_int points; 
 //     vec_vec_int value;
 // } vec_vec_int;
 
-void getGraph(vec_vec_int &g);
+void getGraph(vec_vec_int& graph, Group group);
+void useBFS(vec_vec_int graph, int start, int total);
+int firstAdjVex(vec_vec_int graph, int u);
+int nextAdjVex(vec_vec_int graph, int u, int w);
 int main() {
     int people, relations;
     vec_grp groups;
-    while(cin >> people >> relations && not (people == 0 and relations == 0)) {
+    while (cin >> people >> relations && not (people == 0 and relations == 0)) {
         Group group;
         group.peopleNum = people;
         group.relationNum = relations;
         // for(int i = 0; i < group.peopleNum)
-        for(int i = 0; i < group.relationNum; i++) {
+        for (int i = 0; i < group.relationNum; i++) {
             int a, b;
             cin >> a >> b;
-            relation temp = {a, b};
+            relation temp = { a, b };
             group.relations.push_back(temp);
         }
         groups.push_back(group);
     }
 
-    for(int question = 0; question < groups.size(); question++) {
-        vec_vec_int graph(groups[question].peopleNum, vec_int(groups[question].peopleNum, 0));
-        for(int i = 0; i < groups[question].peopleNum; i++) {
-            for(int j = 0; j < groups[question].peopleNum; j++) {
-                graph[i][j] = false;
-            }
-        }
+    for (int question = 0; question < groups.size(); question++) {
+        vec_vec_int graph(groups[question].peopleNum, vec_int(groups[question].peopleNum, false));
+        // for (int i = 0; i < groups[question].peopleNum; i++) {
+        //     for (int j = 0; j < groups[question].peopleNum; j++) {
+        //         graph[i][j] = false;
+        //     }
+        // }
         getGraph(graph, groups[question]);
+        for(int i = 0; i < graph.size(); i++) {
+            for(int j = 0; j < graph[i].size(); j++) {
+                cout << setw(3) << graph[i][j] << " ";
+            }
+            cout << endl;
+        }
+        for (int i = 0; i < graph.size(); i++) {
+            useBFS(graph, i, graph.size());
+        }
     }
 }
 
-void getGraph(vec_vec_int &graph, Group group) {
+void getGraph(vec_vec_int& graph, Group group) {
     for (int i = 0; i < group.relationNum; i++) {
         graph[group.relations[i].person_a - 1][group.relations[i].person_b - 1] = true;
         graph[group.relations[i].person_b - 1][group.relations[i].person_a - 1] = true;
     }
 }
 
+void useBFS(vec_vec_int graph, int start, int total) {
+    int notOverSeven = 1;
+    bool* visited = new bool[total + 1];
+    for(int i = 0; i < total + 1; i++) {
+        visited[i] = false;
+    }
+    visited[start] = true;
+    queue<int> q;
+    q.push(start);
+    int* level = new int[total + 1];
+    level[0] = 1;
+    for (int len = 1; len < 6 && !q.empty(); len++) {
+        for (int i = 0; i < level[len - 1]; i++) {
+            int u = q.front();
+            q.pop();
+            for (int w = firstAdjVex(graph, u); w >= 0; w = nextAdjVex(graph, u, w)) {
+                if (!visited[w]) {
+                    visited[w] = true;
+                    notOverSeven++;
+                    level[len]++;
+                    q.push(w);
+                }
+            }
+            
+        }
+    }
+    cout << start + 1 << ": " << setprecision(2) <<  notOverSeven   << endl;
+    delete[] visited;
+    delete[] level;
+}
+
+int firstAdjVex(vec_vec_int graph, int u) {
+    // vector<int> next;
+    for (int j = 0; j < graph[u].size(); j++) {
+        if (graph[u][j] != false) {
+            // next.push_back(j);
+            return j;
+            break;
+        }
+    }
+
+    return -1;
+}
+
+int nextAdjVex(vec_vec_int graph, int u, int w) {
+    // bool to_w = false;
+    for (int j = w + 1; j < graph[u].size(); j++) {
+        if (graph[u][j] == true) {
+            return j;
+            break;
+        }
+        // if (j == w) {
+        //     to_w = true;
+        // }
+    }
+
+    return -1;
+}
--- demo.cpp
+++ demo.cpp
@@ -1,15 +1,117 @@
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct // 图书编号，名字，价格
+{
+    char num[20];
+    char name[50];
+    float price;
+} Book;
+struct Node {
+    Book books;
+    struct Node* next;
+};
+struct Node* createNode(Book x) {
+    struct Node* t;
+    t = (struct Node*)malloc(sizeof(struct Node));
+    t->next = NULL;
+    strcpy(t->books.num, x.num);
+    strcpy(t->books.name, x.name);
+    t->books.price = x.price;
+    return t;
+}
+void init(struct Node** phead) {
+    *phead = NULL;
+}
+void printlist(struct Node* head) {
+    int i = 0;
+    while (head) {
+        printf("%s %s %.2f\n", head->books.num, head->books.name, head->books.price);
+        head = head->next;
+        i++;
+    }
+}
+int insert(struct Node** phead, int k, Book x) {
+    struct Node* p = *phead;
+    struct Node* t;
+    t = createNode(x);
+    if (k < 1)
+        return 0;
+    else if (k == 1) {
+        *phead = t;
+        return 1;
+    }
+    else {
+        int count = 1;
+        while (p && count < k - 1) {
+            p = p->next;
+            count++;
+        }
+    }
 
-void f(vector<int> &v) {
-    v[0] = 3;
+    if (p) {
+        t->next = p->next;
+        p->next = t;
+        return 1;
+    }
+    else
+        return 0;
+}
+int getlength(struct Node* head) {
+    int len = 0;
+    while (head) {
+        len++;
+        head = head->next;
+    }
+    return len;
+}
+int removeNode(struct Node** phead, int k) {
+    if (k < 1) {
+        return 0;
+    }
+    else if (k == 1) {
+        if (*phead) {
+            *phead = (*phead)->next;
+            return 1;
+        }
+        else
+            return 0;
+    }
+    else {
+        struct Node* t;
+        struct Node* p;
+        p = *phead;
+        int count = 1;
+        while (p && count < k - 1) {
+            p = p->next;
+            count++;
+        }
+        if (p == NULL || p->next == NULL)
+            return 0;
+        t = p->next;
+        p->next = t->next;
+        free(t);
+        return 1;
+    }
 }
 int main() {
-    vector<int> s;
-    for(int i = 0; i < 5; i++) 
-        s.push_back(i);
-    f(s);
-    cout << s[0];
+    struct Node* head;
+    init(&head);
+    Book x;
+    int k = 1, a, b;
+    while (1) {
+        scanf("%s %s %f", x.num, x.name, &x.price);
+        if (strcmp(x.name, "0") == 0 && strcmp(x.num, "0") == 0 && x.price == 0)
+            break;
+        insert(&head, k, x);
+        k++;
+    }
+    scanf("%d", &a);
+    if (removeNode(&head, a)) {
+        printf("%d\n", getlength(head));
+        printlist(head);
+    }
+    else
+        printf("抱歉，出库位置非法!");
+    return 0;
 }
